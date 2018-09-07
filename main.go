44cc1d3cabff927c3712a18890db25f12a700490
julioc98 <julioc.santos98@gmail.com>
2018-09-06 21:43:23 -0300

    ok structs[

--- main.go
+++ main.go
@@ -7,26 +7,27 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
-type Profile struct {
-	gorm.Model
-	Name string
-}
+func belongsTo() {
 
-type User struct {
-	gorm.Model
-	Profile   Profile
-	ProfileID uint
-}
+	type Profile struct {
+		gorm.Model
+		Name string
+	}
 
-func main() {
+	type User0 struct {
+		gorm.Model
+		Profile   Profile
+		ProfileID uint
+	}
+
 	db, _ := gorm.Open("sqlite3", "test.db")
 	db = db.Set("gorm:auto_preload", true)
 
 	defer db.Close()
 	db.LogMode(true)
 
-	db.AutoMigrate(&User{}, &Profile{})
-	u := new(User)
+	db.AutoMigrate(&User0{}, &Profile{})
+	u := new(User0)
 	p := new(Profile)
 
 	u.ProfileID = 1
@@ -37,5 +38,87 @@ func main() {
 
 	db.Find(&u)
 	log.Println(u)
+}
 
+func hasMany() {
+
+	type Email struct {
+		gorm.Model
+		Email   string
+		User1ID uint
+	}
+
+	type User1 struct {
+		gorm.Model
+		Emails []Email
+	}
+
+	db, _ := gorm.Open("sqlite3", "test.db")
+	db = db.Set("gorm:auto_preload", true)
+
+	defer db.Close()
+	db.LogMode(true)
+
+	db.AutoMigrate(&User1{}, &Email{})
+	u := new(User1)
+	e := new(Email)
+
+	e.Email = "jc@test.com"
+	e.User1ID = 1
+
+	db.Create(&u)
+	db.Create(&e)
+
+	db.Find(&u)
+	log.Println(u)
+}
+
+func belongsToAndHasMany() {
+	type Profile struct {
+		gorm.Model
+		Name string
+	}
+
+	type Email struct {
+		gorm.Model
+		Email   string
+		User2ID uint
+	}
+
+	type User2 struct {
+		gorm.Model
+		Profile   Profile
+		ProfileID uint
+		Emails    []Email
+	}
+
+	db, _ := gorm.Open("sqlite3", "test.db")
+	db = db.Set("gorm:auto_preload", true)
+
+	defer db.Close()
+	db.LogMode(true)
+
+	db.AutoMigrate(&User2{}, &Profile{}, &Email{})
+	u := new(User2)
+	p := new(Profile)
+	e := new(Email)
+
+	e.Email = "jc@test.com"
+	e.User2ID = 1
+
+	u.ProfileID = 1
+	p.Name = "JCTESTEJC"
+
+	db.Create(&p)
+	db.Create(&e)
+	db.Create(&u)
+
+	db.Find(&u)
+	log.Println(u)
+}
+
+func main() {
+	// belongsTo()
+	// hasMany()
+	belongsToAndHasMany()
 }
